b6044b57859d8341e946b217462f61f380e4ad9a
janahlin <jan.ahlin@gmail.com>
2025-04-04 21:05:33 +0000

    Add resource_prefix to common tags

--- terraform/modules/common/outputs.tf
+++ terraform/modules/common/outputs.tf
@@ -1,24 +1,4 @@
 output "common_tags" {
-  description = "Common tags to be used across all resources"
+  description = "Common tags to be applied to all resources"
   value       = local.common_tags
-}
-
-output "resource_prefix" {
-  description = "Resource prefix to be used across all resources"
-  value       = var.resource_prefix
-}
-
-output "environment" {
-  description = "Environment name"
-  value       = var.environment
-}
-
-output "location" {
-  description = "Region/location for resources"
-  value       = var.location
-}
-
-output "cloud_provider" {
-  description = "Cloud provider being used"
-  value       = var.cloud_provider
 }
--- terraform/modules/common/variables.tf
+++ terraform/modules/common/variables.tf
@@ -1,30 +1,10 @@
 variable "environment" {
-  description = "Environment (dev, staging, prod)"
+  description = "Environment name (e.g., dev, prod)"
   type        = string
-  validation {
-    condition     = contains(["dev", "staging", "prod"], var.environment)
-    error_message = "Environment must be one of: dev, staging, prod"
-  }
-}
-
-variable "cloud_provider" {
-  description = "Cloud provider (azure, oci)"
-  type        = string
-  default     = "azure"
-  validation {
-    condition     = contains(["azure", "oci"], var.cloud_provider)
-    error_message = "Cloud provider must be one of: azure, oci"
-  }
 }
 
 variable "resource_prefix" {
-  description = "Prefix to be used for all resources"
+  description = "Prefix to be used for resource names"
   type        = string
-  default     = "elt"
-}
-
-variable "location" {
-  description = "Azure region or OCI region"
-  type        = string
-  default     = "westeurope" # Default for Azure
+  default     = "cloud-elt"
 }
